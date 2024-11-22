198e81dd8e724a7ffca4711a9a346c1232c4e52c
Riya Bharti <121962899+bhartiriya@users.noreply.github.com>
2024-11-22 13:40:44 +0530

    updated default port

--- app.py
+++ app.py
@@ -353,3 +353,5 @@ def remove_from_wishlist(item_id):
 
 if __name__ == "__main__":
     app.run(debug=True)
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host="0.0.0.0", port=port, debug=True)
